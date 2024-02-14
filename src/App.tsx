831ea4bc79a6e836ef878e852d952bec21a94f41
JerryU <jerry.uusitalo@gmail.com>
2024-02-14 15:15:05 +0200

    Add max width for App.tsx container

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Container, Typography } from "@mui/material"
-import Calculator from "./components/Calculator"
+import { Container, Typography } from "@mui/material";
+import Calculator from "./components/Calculator";
 
 const App: React.FC = () => {
-
   return (
-    <Container sx={{height:"100vh", width: "100vw", display:"flex", flexDirection:"column", justifyContent: "center", alignItems:"center"}}>
-        <Typography variant="h1">React/TS Calculator</Typography>
-        <Calculator />
+    <Container
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        maxWidth: "500px !important"
+      }}
+    >
+      <Typography variant="h1">React/TS Calculator</Typography>
+      <Calculator />
     </Container>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
